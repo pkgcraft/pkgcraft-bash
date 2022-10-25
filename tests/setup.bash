96e66e7807ce03dc250ebab56b3a46b87cc1d226
Tim Harder <radhermit@gmail.com>
2022-10-24 22:41:27 -0600

    tests: minor bash plugin loading simplifications

--- tests/setup.bash
+++ tests/setup.bash
@@ -1,14 +1,5 @@
 bats_require_minimum_version 1.7.0
 
-# determine builtin names from test files
-shopt -s failglob
-ALL_BUILTINS=()
-for f in ${BATS_TEST_DIRNAME}/*.bats; do
-	f=${f##*/}
-	ALL_BUILTINS+=( ${f%%.*} )
-done
-unset f
-
 # determine pkgcraft shared library path
 if [[ ${OSTYPE} == "darwin"* ]]; then
 	obj_ext="dylib"
@@ -20,5 +11,7 @@ unset obj_ext
 
 # load builtins during test initialization
 setup() {
-	enable -f ${SHARED_LIBRARY} ${ALL_BUILTINS[*]}
+	local builtin=${BATS_TEST_FILENAME##*/}
+	builtin=${builtin%%.*}
+	enable -f ${SHARED_LIBRARY} ${builtin}
 }
