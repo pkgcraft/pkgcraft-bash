0285998dd348684019ad2b0e1926c6fdf949c193
Tim Harder <radhermit@gmail.com>
2022-10-24 22:20:44 -0600

    tests: determine builtin names from test files instead of hardcoding

--- tests/setup.bash
+++ tests/setup.bash
@@ -1,13 +1,24 @@
 bats_require_minimum_version 1.7.0
 
-ALL_BUILTINS=( atom atom_version profile )
-if [[ ${OSTYPE} == "darwin"* ]]; then
-	OBJ_EXT="dylib"
-else
-	OBJ_EXT="so"
-fi
-SHARED_LIBRARY=${CARGO_BUILD_DIR:-./target/debug}/libpkgcraft_bash.${OBJ_EXT}
+# determine builtin names from test files
+shopt -s failglob
+ALL_BUILTINS=()
+for f in ${BATS_TEST_DIRNAME}/*.bats; do
+	f=${f##*/}
+	ALL_BUILTINS+=( ${f%%.*} )
+done
+unset f
 
+# determine pkgcraft shared library path
+if [[ ${OSTYPE} == "darwin"* ]]; then
+	obj_ext="dylib"
+else
+	obj_ext="so"
+fi
+SHARED_LIBRARY=${CARGO_BUILD_DIR:-./target/debug}/libpkgcraft_bash.${obj_ext}
+unset obj_ext
+
+# load builtins during test initialization
 setup() {
 	enable -f ${SHARED_LIBRARY} ${ALL_BUILTINS[*]}
 }
